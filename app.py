228a39cc98f95d52f74aa7e08bb7ba5b161100a2
Jeremy <yerimahjeremy@gmail.com>
2025-10-05 01:50:57 +0100

    Modify app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from config import Config
 from models import Base, User, ImageUpload
 from utils import FileUtils
 from controllers import HealthController, AuthController, ImageController
+import os
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -46,4 +47,5 @@ def create_app(config_class=Config):
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
